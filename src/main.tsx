0ff73fefb34f7f05fce6255e016b060e2e4ef746
charry07 <charry072013@gmail.com>
2023-07-10 12:01:10 -0500

    configuracion del build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Store from './store/Store';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
